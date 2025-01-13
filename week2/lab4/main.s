40ebf3944c4e07ccba9d1ab5fc5ea66aff27d71d
Sam <samueldavidanderson2001@gmail.com>
2025-01-13 11:44:02 -0800

    adding lab 4 bru

---
+++ week2/lab4/main.s
@@ -0,0 +1,21 @@
+//We starting soon 
+.global _start
+
+//read only
+.section .rodata 
+
+//math const & strings that dont change 
+.section .data
+
+//my string data for "Hello World!\n"
+myTxt: .ascii "Hello World\n"
+
+//var init 
+.section .bss 
+
+//code start
+.text 
+
+_start: 
+	
+
