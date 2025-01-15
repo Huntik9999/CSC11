e0be19aad589badf1c50910745a65f10db244e51
Sam-Pi4 <samueldavidanderson2001@gmail.com>
2025-01-14 23:10:07 -0800

    book work

--- book/chapter3/linkFiles/part1.s
+++ book/chapter3/linkFiles/part1.s
@@ -1,6 +1,7 @@
+/*part1.s file*/
     .global _start @prog3a/s -- a simple assembler file 
 
 _start:
     MOV R0, #65
-    MOV R7, #1
-    SWI 0
+    BAL _part2
+    
---
+++ book/chapter3/linkFiles/part2.s
@@ -0,0 +1,7 @@
+/* paart2.s file */
+    .global _part2 @prog3a/s -- a simple assembler file 
+
+_part2:
+    MOV R7, #7
+    SWI 0
+    
