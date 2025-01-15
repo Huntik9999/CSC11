40aa3754f1a8652884a014ed0645cfc19b4b26da
Sam <samueldavidanderson2001@gmail.com>
2025-01-15 09:34:30 -0800

    update

---
+++ week2/1-15/syscall/main.s
@@ -0,0 +1,31 @@
+.global  _start
+
+.section .data 
+	input: .ascii "Hello World String\n"
+
+
+.text
+_start:
+	mov r7, #3 //read syscall
+
+	mov r0, #0 //stand in
+
+	ldr r1, =input //input address
+
+	mov r2, #5 //amount to input
+
+	swi 0 //syscall
+
+	mov r7, #4 //write syscall
+
+	mov r0, #1 //stdout
+
+	ldr r1, =input //what to stdout
+
+	mov r2, #19 //amount to write
+
+	swi 0 //syscall
+
+	mov r7, #1//
+
+	mov r0, #0//
