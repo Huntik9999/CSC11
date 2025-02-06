e8b34a90826e7d1fcea521b3d8624fabbc7a4a43
Sam-Pi4 <samueldavidanderson2001@gmail.com>
2025-02-05 22:59:56 -0800

    please commit

--- labs/lab15/lab15.s
+++ labs/lab15/lab15.s
@@ -31,7 +31,7 @@ main:
         //counter i = 0 
         mov r4, #1 
         _for:
-            //when i = 10 loop ; zero flag 
+            //when i = 11 loop ; zero flag 
             cmp r4, #11
             bge _end
             //prompt for input 
@@ -58,17 +58,50 @@ main:
     mov r1, #10
     bl arrFunct
     //exit 
+    mov r0, #0
     pop {pc}
 
+//r0 = array; r1 = size 
 arrFunct:
-    push {lr}
+    push {r4, r5, lr}
+    //first arr number 
+    ldr r4, [r0] //big 
+    ldr r5, [r0] //small
     //loop 
     loop:
         //check if loop end 
-        cmp r1, 0
+        cmp r1, #0
         beq exit 
+        
+        //dec loop num by 1
+        sub r1, r1, #1
+        //go to next number in arr 
+        add r0, r0, #4
+        //store array number 
+        ldr r2, [r0]
+        //compare array to small 
+        cmp r5, r2 
+        bge _small
+        //compare array to big 
+        cmp r4, r2
+        ble _big
+        bal loop
+        _big:
+            ldr r4, [r0]
+            bal loop
+        _small:
+            ldr r5, [r0]
+            bal loop
+    exit:
+        //output big number 
+        ldr r0, =coutLarge
+        mov r1, r4
+        bl printf
+        //output small number 
+        ldr r0, =coutSmall
+        mov r1, r5 
+        bl printf 
 
-
-    pop {pc}
+    pop {r4, r5, pc}
 
 
